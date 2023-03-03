691b02ba801919b364002c6f09cef26a6878f6cc
eisha99 <eishafnu@gmail.com>
2023-03-03 15:57:42 -0800

    port

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ load_dotenv()
 if __name__ == '__main__':
     app.run()
 
+#Make sure your application is correctly binding to the $PORT environment variable. 
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
+
 #heroku logs --app studyabroadcircle 
