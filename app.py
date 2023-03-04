acb7917313ad054efe920d970f0a60061f222028
eisha99 <eishafnu@gmail.com>
2023-03-03 16:03:31 -0800

    port 2

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    #Make sure your application is correctly binding to the $PORT environment variable. 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
-#Make sure your application is correctly binding to the $PORT environment variable. 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+
 
 #heroku logs --app studyabroadcircle 
