4169dd94bfe8fe4a668f6adb0d513646463e6f6b
eisha99 <eishafnu@gmail.com>
2023-03-04 17:17:56 -0800

    comments

--- app/db_models.py
+++ app/db_models.py
@@ -11,15 +11,15 @@ class User(db.Model):
     password = db.Column(db.String, nullable  = False) # nullable prevents from having empty inputs
 
 
-class Task(db.Model):
-    """
-        Creates a Task table that is connected to user table with one to many relation (one user, many tasks).
-    """
-    __tablename__ = 'tasks'
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String, db.ForeignKey('users.username'))
-    title = db.Column(db.String(100), nullable = False) # 100 is max length of input sitnrg
-    status = db.Column(Enum('to_do', 'doing', 'done'))
+# class Task(db.Model):
+#     """
+#         Creates a Task table that is connected to user table with one to many relation (one user, many tasks).
+#     """
+#     __tablename__ = 'tasks'
+#     id = db.Column(db.Integer, primary_key = True)
+#     username = db.Column(db.String, db.ForeignKey('users.username'))
+#     title = db.Column(db.String(100), nullable = False) # 100 is max length of input sitnrg
+#     status = db.Column(Enum('to_do', 'doing', 'done'))
 
 # Initialize database 
 db.create_all()
